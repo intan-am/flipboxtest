91cab2c5f935f108231cd701b3d556b34bbc570d
Intan Amalia Sari <intanianz.amalia@gmail.com>
2016-05-26 18:07:06 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Page Title</title>
+</head>
+<body>
+
+	<h1>Welcome</h1>
+	<p>May I help you?</p>
+	<form action="search.php" method="post">
+		Hotel di Kota: <input type="text" name="name">
+		<input type="submit" name="submit">
+	</form>
+
+</body>
+</html>
